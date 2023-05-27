45a880b8cd15692fd7ef3f96d9b076df2959cb74
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-05-27 19:37:50 +0600

    Update function-grogro.js

--- function-grogro.js
+++ function-grogro.js
@@ -1,9 +1,4 @@
 //javascript file for Ecwid store "grogro.no"
 //custom-app to add additional functionality related chilled and unchilled products delivery zones.
 console.log("hello from github!");
-var storeData = EcwidApp.getPayload();
-
-var storeId = storeData.store_id;
-var accessToken = storeData.access_token;
-
-console.log(accessToken);
+alert('hi');
