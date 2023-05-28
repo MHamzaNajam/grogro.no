764d5b3594d02d1faec9a9ea9504da5a25ca0b4a
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-05-29 02:52:56 +0600

    Update function-grogro.js

--- function-grogro.js
+++ function-grogro.js
@@ -116,7 +116,7 @@ async function main()
   const LinkButton = document.createElement('button');
   LinkButton.textContent = 'Finn en butikk';
   LinkButton.style.color = 'white';
-  LinkButton.style.width = '100%';
+  LinkButton.style.width = '80%';
   LinkButton.style.backgroundColor = '#191919';
   LinkButton.style.textAlign = 'center';
   LinkButton.style.paddingLeft = '20px';
