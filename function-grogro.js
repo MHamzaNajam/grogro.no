63828fc1dcb63fbf0ab0307742411f55c0b3b838
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-05-29 03:07:37 +0600

    Update function-grogro.js

--- function-grogro.js
+++ function-grogro.js
@@ -187,11 +187,13 @@ async function main()
       warningParagraph.textContent = '';
       addToCartButton.style.display = "block";
       LinkButton.style.display = "none";
+      if(isSomethingInCart() && checkoutButton) checkoutButton.style.marginTop = '0px';
     }
     else {
        warningParagraph.textContent = 'Vår leveringspartner kan ikke levere kjølevarer til din adresse helt enda. Klikk på knappen under for å se hvor GroGro er tilgjengelig i butikk';
        addToCartButton.style.display = "none";
        LinkButton.style.display = "block";
+      if(isSomethingInCart() && checkoutButton) checkoutButton.style.marginTop = '20px';
     }
     
       if((!value) || (value && value.length < 4)) {
