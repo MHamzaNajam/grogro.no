8be3c531ee43489ed01afd9e813b7f2304da43fd
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-06-19 23:44:06 +0600

    Update function-grogro.js

--- function-grogro.js
+++ function-grogro.js
@@ -249,28 +249,21 @@ async function main()
 
 function executeOnEcwidAvailable(intervalMs) {
 
-  let pageLoadFound = false;
   let pageLoadedFound = false;
   
   function checkEcwidAvailability() {
     
     if (typeof window.Ecwid !== 'undefined') {
-      
-      if(Ecwid?.OnPageLoad && !pageLoadFound) {
-        console.log('page load found, add handler for page Load');
-        Ecwid.OnPageLoad.add(function () { console.log('load'); main(); });
-        pageLoadFound = true;
-      }
        if(Ecwid?.OnPageLoaded && !pageLoadedFound) {
-        console.log('page loaded found, add handler for page Loaded');
-        Ecwid.OnPageLoaded.add(function () { console.log('loaded'); main(); });
+        console.log('pageloaded module found, add handler to the loaded event!');
+        Ecwid.OnPageLoaded.add(function () { console.log('handler added, call main!'); main(); });
         pageLoadedFound = true;
       }
 
     }
   }
   var intervalId = setInterval(checkEcwidAvailability, intervalMs);
-  if(pageLoadFound  && pageLoadedFound) clearInterval(intervalId);    
+  if(pageLoadedFound) clearInterval(intervalId);    
 }
 
 
