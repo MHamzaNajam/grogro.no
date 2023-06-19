5e94f252eb38528844e4445707dd59f6dc716022
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-06-19 22:13:07 +0600

    Update function-grogro.js

--- function-grogro.js
+++ function-grogro.js
@@ -1,18 +1,32 @@
-function checkEcwidAvailability(callback) {
-  var interval = setInterval(function() {
-    if (typeof window.Ecwid !== 'undefined') {
-      clearInterval(interval); // Stop the interval
+function executeOnEcwidAvailable(callback, intervalMs) {
+  var executed = false; // Flag to track execution status
+
+  function checkEcwidAvailability() {
+    if (typeof window.Ecwid !== 'undefined' && !executed) {
+      executed = true; // Set the flag to true
       callback(); // Call the callback function
     }
-  }, 100);
+  }
+
+  var intervalId = setInterval(checkEcwidAvailability, intervalMs); // Start checking Ecwid availability on the given interval
+
+  // Stop the interval once Ecwid becomes available
+  function stopInterval() {
+    if (executed) {
+      clearInterval(intervalId);
+    }
+  }
+
+  // Stop the interval after a certain duration (optional)
+  setTimeout(stopInterval, 10000); // Change 10000 to the desired duration in milliseconds
 }
 
 // Usage:
-checkEcwidAvailability(function() {
-  // This code will be executed when Ecwid becomes available
+executeOnEcwidAvailable(function() {
+  // This code will be executed whenever Ecwid becomes available
   window.Ecwid.OnPageLoaded.add(function(page) {
     if (page.type == "SITE") {
       // Do something with the Ecwid store page...
     }
   });
-});
+}, 1000); // Change 1000 to the desired interval duration in milliseconds
