7f866c26b94f3cd1235ed88d1f2fd1512a29c85a
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-06-19 22:24:51 +0600

    Update function-grogro.js

--- function-grogro.js
+++ function-grogro.js
@@ -1,34 +1,13 @@
-function executeOnEcwidAvailable(callback, intervalMs) {
+function executeOnEcwidAvailable(intervalMs) {
   var executed = false;
   function checkEcwidAvailability() {
-    if (typeof window.Ecwid !== 'undefined' && typeof window.Ecwid.OnPageLoadeded !== 'undefined' &&  typeof window.Ecwid.OnPageLoad !== 'undefined' && !executed) {
-      executed = true; 
-      callback();
+    if (typeof window.Ecwid !== 'undefined') {
+      console.log('oh yee?', window.Ecwid);
     }
   }
-
   var intervalId = setInterval(checkEcwidAvailability, intervalMs);
-
-
-  function stopInterval() {
-    if (executed) {
-      clearInterval(intervalId);
-    }
-  }
-
-  // Stop the interval after a certain duration (optional)
-  setTimeout(stopInterval, 10000); // Change 10000 to the desired duration in milliseconds
+  // clearInterval(intervalId);
 }
 
 
-executeOnEcwidAvailable(function() {
-  // page load
-  window.Ecwid.OnPageLoadeded.add(function(page) {
-    console.log('main should be run');
-  });
-  // page change
-  window.Ecwid.OnPageLoad.add(function(page) {
-    console.log('main should be run');
-  });
-
-}, 1000); 
+executeOnEcwidAvailable(100); 
